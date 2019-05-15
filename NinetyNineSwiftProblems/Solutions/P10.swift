e87085eb3765da13c978ba734ea81da1668289db
Uday <uday.pandey@gmail.com>
2019-05-15 14:45:04 +0100

    Updated README

--- NinetyNineSwiftProblems/Solutions/P10.swift
+++ NinetyNineSwiftProblems/Solutions/P10.swift
@@ -8,8 +8,36 @@
 
 import Foundation
 
-extension Node {
+extension Node where T: Equatable {
+    func encode() -> Node<(Int, T)> {
+        var root = self.pack()
+
+        // Using count and subscript which was written earlier to avoid 
+        // duplicating the logic here
+        let rootNode = Node<(Int, T>>(value: Node(value: root.value.count, root.value[0].value))
+        var currentNode = rootNode
+
+        while let next = root.next {
+            let newCurrentNode = Node<(Int, T>>(value: Node(value: next.value.count, next.value[0].value))
+            currentNode.next = newCurrentNode
+            currentNode = newCurrentNode
+            root = next
+        }
+        
+        return rootNode
+    }
 }
 
-extension Array {
+extension Array where Element: Equatable {
+    func encode() -> [(Int, Element)] {
+        var packedList = self.pack()
+
+        var encodedList = [(Int, Element)]()
+
+        packedList.forEach { elms in
+            encodedList.append((elms.count, elm[0]))
+        }
+        
+        return packedList
+    }
 }
--- NinetyNineSwiftProblems/Solutions/P14.swift
+++ NinetyNineSwiftProblems/Solutions/P14.swift
@@ -9,7 +9,36 @@
 import Foundation
 
 extension Node {
+    func duplicate() -> Node<T> {
+        var root = self
+
+        var secondNode = Node(value: next.value)
+        let rootNode = Node(value: next.value, next: secondNode)
+        var currentNode = secondNode
+
+        while let next = root.next {
+            var secondNode = Node(value: next.value)
+            let firstNode = Node(value: next.value, next: secondNode)
+            currentNode.next = firstNode
+            currentNode = secondNode
+
+            root = next
+        }
+
+        return rootNode
+    }
 }
 
 extension Array {
+    func duplicate() -> [Element] {
+        var rootList = [Element]()
+        guard !isEmpty else { return rootList }
+
+        for elm in self {
+            rootList.append(elm)
+            rootList.append(elm)
+        }
+
+        return rootList
+    }
 }
