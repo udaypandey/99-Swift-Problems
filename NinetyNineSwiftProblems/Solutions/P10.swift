1b048138d810db10ba94293f4d5c04917436d08b
Uday <uday.pandey@gmail.com>
2019-05-15 23:05:43 +0100

    Added solution and test cases for P10

--- NinetyNineSwiftProblems/Solutions/P10.swift
+++ NinetyNineSwiftProblems/Solutions/P10.swift
@@ -9,35 +9,37 @@
 import Foundation
 
 extension Node where T: Equatable {
-//    func encode() -> Node<(Int, T)> {
-//        var root = self.pack()
-//
-//        // Using count and subscript which was written earlier to avoid
-//        // duplicating the logic here
-//        let rootNode = Node<(Int, T>>(value: Node(value: root.value.count, root.value[0].value))
-//        var currentNode = rootNode
-//
-//        while let next = root.next {
-//            let newCurrentNode = Node<(Int, T>>(value: Node(value: next.value.count, next.value[0].value))
-//            currentNode.next = newCurrentNode
-//            currentNode = newCurrentNode
-//            root = next
-//        }
-//
-//        return rootNode
-//    }
+    func encode() -> Node<(Int, T)> {
+        var root = self.pack()
+
+        // Using count and subscript which was written earlier to avoid
+        // duplicating the logic here
+        let val = (root.value.count, root.value[0]!)
+        let rootNode = Node<(Int, T)>(value: val)
+        var currentNode = rootNode
+
+        while let next = root.next {
+            let val = (next.value.count, next.value[0]!)
+            let newCurrentNode = Node<(Int, T)>(value: val)
+
+            currentNode.next = newCurrentNode
+            currentNode = newCurrentNode
+            root = next
+        }
+
+        return rootNode
+    }
 }
 
 extension Array where Element: Equatable {
-//    func encode() -> [(Int, Element)] {
-//        var packedList = self.pack()
-//
-//        var encodedList = [(Int, Element)]()
-//
-//        packedList.forEach { elms in
-//            encodedList.append((elms.count, elm[0]))
-//        }
-//        
-//        return packedList
-//    }
+    func encode() -> [(Int, Element)] {
+        let packedList = self.pack()
+        var encodedList = [(Int, Element)]()
+
+        packedList.forEach { elms in
+            encodedList.append((elms.count, elms[0]))
+        }
+
+        return encodedList
+    }
 }
--- NinetyNineSwiftProblemsTests/P10Tests.swift
+++ NinetyNineSwiftProblemsTests/P10Tests.swift
@@ -18,43 +18,68 @@ class P10Tests: XCTestCase {
         super.tearDown()
     }
 
-    func testP0501() {
-        var node = Node(1, 2, 3)
-        var reverseNode = node.reverse()
+    func testP1001() {
+        var node = Node(1, 2, 3, 3, 4, 5, 5)
+        var encodedNode: Node<(Int, Int)> = node.encode()
+        var acceptNode: Node<(Int, Int)> = Node((1, 1), (1, 2), (2, 3), (1, 4), (2, 5))
+        XCTAssertEqual(encodedNode.count, 5, "Invalid encoded value")
+        XCTAssertEqual(encodedNode[0]?.0, acceptNode[0]?.0, "Invalid encoded value")
+        XCTAssertEqual(encodedNode[0]?.1, acceptNode[0]?.1, "Invalid encoded value")
+        XCTAssertEqual(encodedNode[1]?.0, acceptNode[1]?.0, "Invalid encoded value")
+        XCTAssertEqual(encodedNode[1]?.1, acceptNode[1]?.1, "Invalid encoded value")
+        XCTAssertEqual(encodedNode[2]?.0, acceptNode[2]?.0, "Invalid encoded value")
+        XCTAssertEqual(encodedNode[2]?.1, acceptNode[2]?.1, "Invalid encoded value")
+        XCTAssertEqual(encodedNode[3]?.0, acceptNode[3]?.0, "Invalid encoded value")
+        XCTAssertEqual(encodedNode[3]?.1, acceptNode[3]?.1, "Invalid encoded value")
+        XCTAssertEqual(encodedNode[4]?.0, acceptNode[4]?.0, "Invalid encoded value")
+        XCTAssertEqual(encodedNode[4]?.1, acceptNode[4]?.1, "Invalid encoded value")
 
-        // Using from problem 3
-        XCTAssertEqual(reverseNode[0], 3, "Invalid value")
-        XCTAssertEqual(reverseNode[1], 2, "Invalid value")
-        XCTAssertEqual(reverseNode[2], 1, "Invalid value")
+        node = Node(3, 3, 3)
+        encodedNode = node.encode()
+        acceptNode = Node((3, 3))
+        XCTAssertEqual(encodedNode.count, 1, "Invalid encoded value")
+        XCTAssertEqual(encodedNode[0]?.0, acceptNode[0]?.0, "Invalid encoded value")
+        XCTAssertEqual(encodedNode[0]?.1, acceptNode[0]?.1, "Invalid encoded value")
 
-        node = Node(1)
-        reverseNode = node.reverse()
-        XCTAssertEqual(reverseNode[0], 1, "Invalid value")
+        node = Node(3, 3, 3, 4, 4, 4)
+        encodedNode = node.encode()
+        acceptNode = Node((3, 3), (3, 4))
+        XCTAssertEqual(encodedNode.count, 2, "Invalid encoded value")
+        XCTAssertEqual(encodedNode[0]?.0, acceptNode[0]?.0, "Invalid encoded value")
+        XCTAssertEqual(encodedNode[0]?.1, acceptNode[0]?.1, "Invalid encoded value")
+        XCTAssertEqual(encodedNode[1]?.0, acceptNode[1]?.0, "Invalid encoded value")
+        XCTAssertEqual(encodedNode[1]?.1, acceptNode[1]?.1, "Invalid encoded value")
     }
 
-    func testP0502() {
-        var list = [1, 2, 3]
-        list.reverse$()
+    func testP1002() {
+        var list = [1, 2, 3, 3, 4, 5, 5]
+        var encodedList = list.encode()
+        var acceptList = [(1, 1), (1, 2), (2, 3), (1, 4), (2, 5)]
+        XCTAssertEqual(encodedList[0].0, acceptList[0].0, "Invalid encoded value")
+        XCTAssertEqual(encodedList[0].1, acceptList[0].1, "Invalid encoded value")
+        XCTAssertEqual(encodedList[1].0, acceptList[1].0, "Invalid encoded value")
+        XCTAssertEqual(encodedList[1].1, acceptList[1].1, "Invalid encoded value")
+        XCTAssertEqual(encodedList[2].0, acceptList[2].0, "Invalid encoded value")
+        XCTAssertEqual(encodedList[2].1, acceptList[2].1, "Invalid encoded value")
+        XCTAssertEqual(encodedList[3].0, acceptList[3].0, "Invalid encoded value")
+        XCTAssertEqual(encodedList[3].1, acceptList[3].1, "Invalid encoded value")
+        XCTAssertEqual(encodedList[4].0, acceptList[4].0, "Invalid encoded value")
+        XCTAssertEqual(encodedList[4].1, acceptList[4].1, "Invalid encoded value")
 
-        XCTAssertEqual(list[0], 3, "Invalid value")
-        XCTAssertEqual(list[1], 2, "Invalid value")
-        XCTAssertEqual(list[2], 1, "Invalid value")
+        list = [3, 3, 3, 3]
+        encodedList = list.encode()
+        acceptList = [(4, 3)]
+        XCTAssertEqual(encodedList.count, 1, "Invalid encoded value")
+        XCTAssertEqual(encodedList[0].0, acceptList[0].0, "Invalid encoded value")
+        XCTAssertEqual(encodedList[0].1, acceptList[0].1, "Invalid encoded value")
 
-        list = [1]
-        list.reverse$()
-        XCTAssertEqual(list[0], 1, "Invalid value")
-    }
-
-    func testP0503() {
-        var list = [1, 2, 3]
-        var reversedList = list.reversed$()
-
-        XCTAssertEqual(reversedList[0], 3, "Invalid value")
-        XCTAssertEqual(reversedList[1], 2, "Invalid value")
-        XCTAssertEqual(reversedList[2], 1, "Invalid value")
-
-        list = [1]
-        reversedList = list.reversed$()
-        XCTAssertEqual(reversedList[0], 1, "Invalid value")
+        list = [3, 3, 3, 4, 4, 4]
+        encodedList = list.encode()
+        acceptList = [(3, 3), (3, 4)]
+        XCTAssertEqual(encodedList.count, 2, "Invalid encoded value")
+        XCTAssertEqual(encodedList[0].0, acceptList[0].0, "Invalid encoded value")
+        XCTAssertEqual(encodedList[0].1, acceptList[0].1, "Invalid encoded value")
+        XCTAssertEqual(encodedList[1].0, acceptList[1].0, "Invalid encoded value")
+        XCTAssertEqual(encodedList[1].1, acceptList[1].1, "Invalid encoded value")
     }
 }
